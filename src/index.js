b22bf9d78ea25817d2f7a483dd8e2cc63c3814bc
esoshyki <esoshyki@yandex.by>
2019-03-03 17:18:32 +0300

    almost done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var num = number;
+  var keynum = base;
+  var count = 0;
+  var stepen = 2;
+  var arr = true;
+  var half = 1;
+  while (arr==true) {
+    if (keynum == 2) {
+      keynum = 2;
+      break;
+    }   
+    for (i=2; i<keynum; i++) {
+      if (keynum%i == 0) {
+        if (Number.isInteger(keynum**0.5)) {
+        half += 1};
+        if (i*i*i == keynum) { half += 2};
+        keynum = keynum / i;
+        arr = true;
+        break
+      };
+      arr = false
+    };
+  };
+
+ count = Math.floor(num/keynum);
+
+ while (keynum**stepen <= num) {
+   count += Math.floor(num/(keynum**stepen));
+   stepen += 1;
+ }; 
+
+ return Math.floor(count/half)
 }
